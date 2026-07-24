fd36b49dee730ffdc852997994a63b84a72cc80f
Ali Mashaghi <aliu.mashaghi@gmail.com>
2026-07-24 14:51:23 +0330

    adding shop_tags and fixes

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path ,re_path
 from . import views
 app_name ="shop"
 urlpatterns = [
-    path('product/grid/', views.ShopProductGridView.as_view(),name = "product_grid"),
-    re_path(r'(?P<slug>[^/]+)/?$', views.ShopProductDetailView.as_view(), name="post_detail"),
+    path('product/grid/', views.ShopProductGridView.as_view(),name = "product-grid"),
+    re_path(r'(?P<slug>[^/]+)/?$', views.ShopProductDetailView.as_view(), name="post-detail"),
 ]
